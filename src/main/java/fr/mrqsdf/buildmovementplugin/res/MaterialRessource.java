7b21f18d618844e5ab3b6f9d3ebad7faefb49367
mrqsdf <maqw@free.fr>
2024-02-09 15:03:07 +0100

    add multi version

---
+++ src/main/java/fr/mrqsdf/buildmovementplugin/res/MaterialRessource.java
@@ -0,0 +1,10 @@
+package fr.mrqsdf.buildmovementplugin.res;
+
+import org.bukkit.Material;
+
+public class MaterialRessource {
+
+    public static Material hoe;
+    public static Material shovel;
+
+}
